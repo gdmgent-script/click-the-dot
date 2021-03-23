db22f3f3cf6f2de364dd797b8f39d7cf13a22b16
Thrive <hello@thrive.be>
2021-03-23 12:16:27 +0100

    wip

---
+++ app.js
@@ -0,0 +1,57 @@
+/**
+ * Hide element
+ * @argument {object} item
+ */
+const toggleHide = (item)=>{
+    item.classList.toggle('hide');
+}
+
+
+/**
+ * Get element random location
+ * @return {array} [left ,top]
+ */
+ const getRandomLocation = ()=>{
+    //get canvas width and height
+     const cw = window.innerWidth;
+     const ch = window.innerHeight;
+     return [Math.floor(Math.random() * cw), Math.floor(Math.random() * ch)];
+ }
+ 
+/**
+ * Get element random location
+ * @argument {object} item
+ */
+const setReandomLocation = (item) => {
+    //get random location within viewport
+    const dotLocation = getRandomLocation();
+    item.style.left = dotLocation[0]+'px';
+    item.style.top = dotLocation[1]+'px';
+ }
+ 
+
+/**
+ * Initialize dot
+ * 
+ */
+const initDot = () => {
+    //init counter
+    let counter = 0;
+    let counterSpan = document.getElementById('score-span');
+    //initdot
+    const dot = document.createElement('div');
+    dot.classList.add('hideandseek');
+    setReandomLocation(dot);
+    document.body.appendChild(dot);
+
+    //hide and unhide
+    setInterval(() => { toggleHide(dot) }, 1000);
+    //set location
+    setInterval(() => { setReandomLocation(dot) }, 1000);
+    
+    dot.addEventListener('click', () => {
+        counter++;
+        counterSpan.innerText = counter;
+    })
+}
+initDot();
